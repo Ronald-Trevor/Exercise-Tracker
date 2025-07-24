8561c6d0299690f67395f25ca2f8f2edefb7972e
Ronald-Trevor <101838410+Ronald-Trevor@users.noreply.github.com>
2025-07-24 21:04:20 +0300

    Complete backend

--- backend/routes/exercise.route.js
+++ backend/routes/exercise.route.js
@@ -12,6 +12,7 @@ exerciseRoute.route("/").get((req, res) => {
     });
 });
 
+//Add a new exercise
 exerciseRoute.route("/add").post((req, res) => {
   const username = req.body.username;
   const description = req.body.description;
@@ -29,5 +30,49 @@ exerciseRoute.route("/add").post((req, res) => {
       res.status(400).json("Error:" + err);
     });
 });
+
+//Return a single exercise
+exerciseRoute.route("/:id").get((req,res) => {
+  Exercise.findById(req.params.id)
+  .then((exercise)=>{
+    res.json(exercise)
+  })
+  .catch((error)=>{
+    res.status(400).json("Error:" + error);
+  })
+})
+
+//Update a single exercise
+exerciseRoute.route("/update/:id").put((req,res)=>{
+  Exercise.findByIdAndUpdate(req.params.id)
+  .then((exercise) => {
+    exercise.username = req.body.username;
+    exercise.description = req.body.description;
+    exercise.duration = req.body.duration;
+    exercise.date = req.body.date;
+
+    exercise.save()
+    .then(() => {
+      res.json("Exercise updated successfully")
+    })
+    .catch((error) => {
+      res.status(400).json("Error:" + error);
+    })
+  })
+  .catch((error) =>{
+    res.status(400).json("Error:" + error);
+  })
+})
+
+//Delete an exercise
+exerciseRoute.route("/delete/:id").delete((req,res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+  .then(() => {
+    res.json("Deleted exercise successfully");
+  })
+  .catch((error) => {
+    res.status(400).json("Error:" + error);
+  })
+})
 
 export default exerciseRoute;
